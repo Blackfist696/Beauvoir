054a79aaf89eb59a3573d5874349b0863db7d442
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2026-03-20 12:10:49 +0100

    ajout js et factorisation des fichiers html

---
+++ js/main.js
@@ -0,0 +1,44 @@
+if(sessionStorage.getItem('role') === null) {
+    fetch('../html/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
+    fetch('../html/login.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('container').innerHTML = data;
+    });
+} else {
+    fetch('../html/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
+
+if(sessionStorage.getItem('role') === 'administrateur') {
+    fetch('../html/navAdmin.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('nav').innerHTML = data;
+    });
+} else if(sessionStorage.getItem('role') === 'administration') {
+  fetch('../html/navGestion.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('nav').innerHTML = data;
+    });
+} else if(sessionStorage.getItem('role') === 'user') {
+  fetch('../html/navUser.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('nav').innerHTML = data;
+    });
+}
+
+  fetch('../test/dashboard.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('container').innerHTML = data;
+    });
+}
